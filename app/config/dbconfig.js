a7923a90c82292823c43647605820c7eba1240c7
Dong Zhao <dzhao@redhat.com>
2019-05-28 16:50:08 +0800

    removed useless files

--- app/config/dbconfig.js
+++ app/config/dbconfig.js
@@ -8,22 +8,8 @@ let sqlite3 = require('sqlite3').verbose();
 /* Load database file (Creates file if not exists) */
 let db = new sqlite3.Database('db/sqlite.db');
 
-/* Init car and driver tables if they don't exist */
+/* Init */
 let init = function () {
-    db.run("CREATE TABLE if not exists car (" +
-        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-        " maker TEXT," +
-        " model TEXT," +
-        " year INT," +
-        " driver INT" +
-        ")");
-
-    db.run("CREATE TABLE if not exists driver (" +
-        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-        " firstName TEXT," +
-        " lastName TEXT," +
-        " car INT" +
-        ")");
 };
 
 module.exports = {
--- app/routes/router.js
+++ app/routes/router.js
@@ -5,8 +5,6 @@ const express = require('express');
 const router = express.Router();
 
 /* API routes */
-router.use('/car', require('./api/carRoutes'));
-router.use('/driver', require('./api/driverRoutes'));
 router.use('/user', require('./api/userRoutes'));
 router.use('/link', require('./api/linkRoutes'));
 router.use('/tag-link', require('./api/tag_linkRoutes'));
