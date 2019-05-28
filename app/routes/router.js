48997472cdaac3c7f74d3cd436d2b25e706d87f5
Dong Zhao <dzhao@redhat.com>
2019-05-28 22:32:02 +0800

    renamed tag_link route to tag route

--- app/routes/router.js
+++ app/routes/router.js
@@ -7,6 +7,6 @@ const router = express.Router();
 /* API routes */
 router.use('/user', require('./api/userRoutes'));
 router.use('/link', require('./api/linkRoutes'));
-router.use('/tag-link', require('./api/tag_linkRoutes'));
+router.use('/tag', require('./api/tagRoutes'));
 
 module.exports = router;
